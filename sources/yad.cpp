4ef2683d2e88f14f0c442fff405c0885870e3778
rusdevops <rusdevops@gmail.com>
2017-05-29 22:49:51 +0300

    added methods

--- sources/yad.cpp
+++ sources/yad.cpp
@@ -5,6 +5,7 @@ using namespace yadisk;
 namespace po = boost::program_options;
 
 #include <cstdlib>
+#include <exception>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -13,57 +14,123 @@ int main(int argc, char* argv[]) {
 
     desc.add_options()
     ("help", "produce help message")
-    ("token", "oAuth token");
+    ("token", po::value<std::string>(), "oAuth token")
+    ("force,f", po::value<bool>(), "")
+    ("command", po::value<std::string>(), "command")
+    ("resource", po::value<std::string>(), "resource")
+    ("location", po::value<std::string>(), "location");
+    
 
     po::positional_options_description p;
         
-    p.add("command", 1).add("resource", 2);
+    p.add("command", 1).add("resource", 1).add("location", 1);
 
     po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).
-              options(desc).positional(p).run(), vm);
+    auto parsed = po::command_line_parser(argc, argv).style(po::command_line_style::short_allow_adjacent).
+              options(desc).positional(p).run();
+    po::store(parsed, vm);
     po::notify(vm);
 
-    if(vm.count("command")) {
-        auto command = vm["command"].as<std::string>();
-        if (command == "help") {
-            std::cout << desc << std::endl;   
-        }
-        else if (command == "version") {
-            std::cout << YDCLI_VERSION_STRING << std::endl;
+    bool force = vm.count("force");
+
+    try {
+
+        if(vm.count("command")) {
+            auto command = vm["command"].as<std::string>();
+            if (command == "help") {
+                std::cout << desc << std::endl;   
+            }
+            else if (command == "version") {
+                std::cout << YDCLI_VERSION_STRING << std::endl;
+            }
+            else {
+                std::string token;
+                if (vm.count("token")) {
+                    token = vm["token"].as<std::string>();
+                } 
+                else {
+                    token = std::getenv("YAD_TOKEN");
+                }
+                
+                json answer;
+                Client client{ token }; 
+
+                if (command == "ls") {
+                    // TODO answer = client.info("/");
+                }
+                else if (command =="status") {
+                    std::cout << "server is " << (client.ping() ? "available" : "not available") << std::endl;
+                }
+                else if (command == "info") {
+                    if (vm.count("resource")) {
+                        auto resource = vm["resource"].as<std::string>();
+                        // TODO answer = client.info(resource);
+                    }
+                    else {
+                        answer = client.info();
+                    }
+                }       
+                else if (command == "mkdir") {
+                    if (vm.count("resource")) {
+                        auto path = vm["resource"].as<std::string>();
+                        answer = client.mkdir(path);
+                    }
+                    else throw std::invalid_argument("resource");
+                }
+                else if (command == "remove") {
+                    if (vm.count("resource")) {
+                        auto path = vm["resource"].as<std::string>();
+                        answer = client.remove(path, force);
+                    }
+                    else throw std::invalid_argument("resource");
+                }
+                else if (command == "publish") {
+                    if (vm.count("resource")) {
+                        auto path = vm["resource"].as<std::string>();
+                        answer = client.publish(path);
+                    }
+                    else throw std::invalid_argument("resource");
+                }
+                else if (command == "unpublish") {
+                    if (vm.count("resource")) {
+                        auto path = vm["resource"].as<std::string>();
+                        answer = client.unpublish(path);
+                    }
+                    else throw std::invalid_argument("resource");
+                }
+                else if (command == "copy") {
+                    if (vm.count("resource")) {
+                        if (vm.count("location")) {
+                            auto from = vm["resource"].as<std::string>();
+                            auto to = vm["location"].as<std::string>();
+                            client.copy(from, to, force);
+                        }
+                        else throw std::invalid_argument("location");
+                    }
+                    else throw std::invalid_argument("resource");
+                }
+
+                else if (command == "move") {
+                    if (vm.count("resource")) {
+                        if (vm.count("location")) {
+                            auto from = vm["resource"].as<std::string>();
+                            auto to = vm["location"].as<std::string>();
+                            client.move(from, to, force);
+                        }
+                        else throw std::invalid_argument("location");
+                    }
+                    else throw std::invalid_argument("resource");
+                }
+                if (! answer.empty()) {
+                    std::cout << answer.dump(4) << std::endl;
+                }
+            }
         }
         else {
-            std::string token;
-            if (vm.count("token")) {
-                token = vm["token"].as<std::string>();
-            } 
-            else {
-                token = std::getenv("YAD_TOKEN");
-            }
-    
-            Client client{ token }; 
-
-            if (command == "ls") {
-                auto info = client.info();
-                std::cout << info.dump(4) << std::endl;
-            }
-            else if (command =="status") {
-                std::cout << "server is " << (client.ping() ? "available" : "not available") << std::endl;
-            }
-            else if (command == "info") {
-                if (vm.count("resource")) {
-                    auto info = client.info();
-                    std::cout << info.dump(4) << std::endl;
-                }
-                else {
-                    auto resource = vm["resource"].as<std::string>();
-                    auto info = client.info(resource);
-                    std::cout << info.dump(4) << std::endl;
-                }
-            }       
+            std::cout << desc << std::endl; 
         }
     }
-    else {
+    catch(std::exception& exc) {
         std::cout << desc << std::endl; 
     }
 }
